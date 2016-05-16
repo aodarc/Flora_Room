82ecacab732594d6458922561633116718ffddc6
sanchalex <odarchenko@ex.ua>
2016-05-16 22:43:51 +0300

    add base template and modified settings

--- Flora_Room/urls.py
+++ Flora_Room/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.shortcuts import render
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', lambda r: render(r, 'main.html'))
 ]
