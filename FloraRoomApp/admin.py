742397f58eb3190a654c9821d1f6e87733fcfbca
sanchalex <odarchenko@ex.ua>
2016-05-20 00:43:59 +0300

    added Models, fix navbar, reviews articles in main Page, modified admin page

--- FloraRoomApp/admin.py
+++ FloraRoomApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from FloraRoomApp import models
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['title', 'text', 'author', 'category']
+    list_filter = ['create_date']
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
+admin.site.register(models.Category)
--- FloraRoomApp/views.py
+++ FloraRoomApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from FloraRoomApp import models
 
 # Create your views here.
+
+
+def main_page(request):
+    context = {'short_articles': models.Article.objects.all()}
+
+    return render(request, 'mainPage.html', context)
